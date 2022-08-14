7fa8b0a42dd07f3b2e6942b480d0c30ba9c8bd7c
Fishman <98775292+Fishman16180@users.noreply.github.com>
2022-08-14 19:05:09 +0200

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,19 @@
+num = int(input("Enter the number of items to generate Fibonacci series: "))
+
+n1 = 0
+n2 = 1
+count = 0
+
+if num <= 0:
+    print("Please enter a positive integer")
+elif num == 0:
+    pass
+elif num == 1:
+    print(n1)
+else:
+    while count < num:
+        print(n1)
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        count += 1
